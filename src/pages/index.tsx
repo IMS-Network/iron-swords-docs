8bfe93623279c86a304a73740dbebd02ed8dbd9e
thefourCraft <me@thefourcraft.com>
2024-05-01 19:45:08 +0300

    Update AboutHomePage component and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import AboutHomePage from '@site/src/components/AboutHomePage';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
@@ -41,6 +42,7 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
+        <AboutHomePage />
         <HomepageFeatures />
       </main>
     </Layout>
